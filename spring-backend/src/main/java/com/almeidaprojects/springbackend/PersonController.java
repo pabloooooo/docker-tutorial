39d326920780eebec02c6bc5136685cd820e72bd
Pablo Almeida <pablo@almeidaprojects.com>
2024-04-23 23:16:20 -0400

    frontend done

--- spring-backend/src/main/java/com/almeidaprojects/springbackend/PersonController.java
+++ spring-backend/src/main/java/com/almeidaprojects/springbackend/PersonController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin(origins = "*")
 public class PersonController {
     private final PersonRepository personRepository;
 
