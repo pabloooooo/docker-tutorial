1f8293b47f1530fb9f85c37516eb07cf9bef4d3e
Pablo Almeida <pablo@almeidaprojects.com>
2024-04-23 21:23:40 -0400

    init

---
+++ spring-backend/src/test/java/com/almeidaprojects/springbackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almeidaprojects.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
